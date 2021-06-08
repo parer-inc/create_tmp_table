dff18c9b4e33fbdaeee068e8f1e73d39cb93f1c5
max <maxfy@gmail.com>
2021-06-08 16:04:45 +0300

    dock upd

--- create_tmp_table.py
+++ create_tmp_table.py
@@ -6,6 +6,7 @@ import MySQLdb
 from rq import Worker, Queue, Connection
 from methods.connection import get_redis, get_cursor
 
+r = get_redis()
 
 def create_tmp_table(name):
     """Create new tmp table"""
@@ -33,8 +34,6 @@ def create_tmp_table(name):
 
 
 if __name__ == '__main__':
-    time.sleep(5)
-    r = get_redis()
     q = Queue('create_tmp_table', connection=r)
     with Connection(r):
         worker = Worker([q], connection=r, name='create_tmp_table')
